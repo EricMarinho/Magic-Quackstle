74263d01aad50846d6b9b89416ae41e58a667c96
Eric Marinho (Ilidam) <ericmarinho@acad.ifma.edu.br>
2023-05-19 22:11:05 -0300

    first commit

---
+++ Assets/Data/Player/PlayerData2D.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[AddComponentMenu("Data/Player/PlayerData2D",order:1)]
+public class PlayerData2D : ScriptableObject
+{
+    public float _speed;
+}
---
+++ Assets/Data/Player/PlayerData3D.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[AddComponentMenu("Data/Player/PlayerData3D", order: 0)]
+public class PlayerData3D : ScriptableObject
+{
+    public float _speed;
+}
