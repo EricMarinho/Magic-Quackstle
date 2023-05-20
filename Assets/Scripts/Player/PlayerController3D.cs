e9b744a6892de1a2b3522fe66e5b7886081cc027
Eric Marinho (Ilidam) <ericmarinho@acad.ifma.edu.br>
2023-05-20 00:42:41 -0300

    feat: hide mouse when in 3D

--- Assets/Scripts/Player/PlayerController3D.cs
+++ Assets/Scripts/Player/PlayerController3D.cs
@@ -1,18 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController3D : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
-        
+        HideAndLockMouse();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideAndLockMouse()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 }
